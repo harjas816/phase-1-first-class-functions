133954a3828da197ed76a2a7cd0d18693e06310e
harjas816 <harjas816@gmail.com>
2023-05-26 13:02:32 -0400

    complete

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function receivesAFunction(callback){
+    callback()
+}
+
+
+
+function returnsANamedFunction(){
+    return function hello(){
+        console.log("hello")
+    }
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("bruh")
+    }
+}
